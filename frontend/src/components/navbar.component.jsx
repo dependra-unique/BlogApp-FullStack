3b7959ab3f81977320c0c179cbb0ab1068b2264c
Dependra Kumar <dependragitcreator@gmail.com>
2024-07-04 23:21:39 +0530

    add functionality of search bar

--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../imgs/youtube2 logo.jpg';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+
+    const [searchBoxVisibility , setSearchBoxVisibility] = useState(false);
+
   return (
     <nav className='navbar'>
       <Link to="/" className='flex-none w-20'>                  
         <img src={Logo} className='w-full' alt='YouTube Logo'/>
       </Link>
 
-      <div className='absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto'>
+      <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto "
+       + (searchBoxVisibility ? "show" : "hide")
+      }>
         <input 
           type='text'
           placeholder='Search'
@@ -17,6 +22,14 @@ function Navbar() {
         />
 
         <i className="fi fi-rr-search absolute right-[10%] md:right-auto md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+      </div>
+
+      <div className='flex items-center gap-3 md:gap-6 ml-auto'>
+        <button className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center'
+        onClick={() => setSearchBoxVisibility(currentValue => !currentValue)}
+        >
+            <i class="fi fi-rr-search text-xl"></i>
+        </button>
       </div>
     </nav>
   );
