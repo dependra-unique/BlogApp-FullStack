0e31af1cfb582d958152844cf00d78e9d9a833e5
Dependra Kumar <dependragitcreator@gmail.com>
2024-07-06 23:53:11 +0530

    continue with google button setup

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import InputBox from '../components/InputBox.jsx';
+import googleIcon from "../imgs/google.png"
 
 function UserAuthForm({type}) {
 
@@ -6,11 +8,11 @@ function UserAuthForm({type}) {
     <section className='h-cover flex items-center justify-center'>
         <form className='w-[80%] max-w-[400px]'>
           <h1 className='text-4xl font-gelasio capitalize text-center mb-24'> 
-            {type == "signin" ? "Welcome Back" : "Join Us Today"}
+            {type == "sign-in" ? "Welcome Back" : "Join Us Today"}
           </h1>
 
           {
-            type != "signin" ?  
+            type != "sign-in" ?  
             <InputBox 
               name="fullname"
               type="text"
@@ -33,9 +35,40 @@ function UserAuthForm({type}) {
               icon="fi-rr-key"
             />        
 
-            <button className='btn-dark center mt-19 px-4'>
-              { type }
+            <button className='btn-dark center mt-19 px-4'
+              type='submit'
+            >
+              { type.replace("-", " ")}
             </button>
+
+            <div className='relative w-full flex items-center gap-2 my-10 opacity-20 uppercase text-black font-bold'>
+                <hr className='w-1/2 border-black' />
+                <p>or</p>
+                <hr className='w-1/2 border-black' />
+            </div>
+
+            <button className='btn-dark px-5 flex items-center justify-center gap-4 w-[90%] center'>
+                <img src={googleIcon} className='w-5'/>
+                continue with google
+            </button>
+
+            {
+
+                type == "sign-in" ? 
+                <p className='mt-6 text-dark-grey text-xl text-center'>
+                  Don't have an account ?
+                  <Link to="/signup" className=' underline text-center text-xl ml-1 hover:no-underline'>
+                    join us today
+                  </Link>
+                </p>
+                :
+                <p className='mt-6 text-dark-grey text-xl text-center'>
+                Already a member ?
+                <Link to="/signin" className=' underline text-center text-xl ml-1 hover:no-underline'>
+                  sign in here.
+                </Link>
+              </p>
+            }
         </form>
       </section>
   )
