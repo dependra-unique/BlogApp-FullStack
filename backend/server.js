77b9552b4ace6cac521d2508224ec201d89576e3
Dependra Kumar <dependragitcreator@gmail.com>
2024-07-09 00:10:29 +0530

    password hashes using bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-import "dotenv/config"
+import "dotenv/config";
+import bcrypt from "bcrypt";
 
 const app = express();
 let port = process.env.PORT || 3000;
@@ -29,9 +30,18 @@ app.post("/signup", (req, res) => {
         return res.status(403).json({"error": "email invalid"});
     }
     if(!passwordRegex.test(password)){
-        return res.status(403).json({"error": "invalid password"})
+        return res.status(403).json({"error": "password should be at least 6 to 20 characters with long a numeric, 1 lowercase and 1 uppercase"})
     }
     res.status(200).json({"status" : "okey"});
+
+
+
+    //password hashes and stored in DB
+    bcrypt.hash(password, 10, (err, hashPassword) => {
+        console.log(hashPassword);
+    })
+
+
 })
 
 app.listen(port, () => {
