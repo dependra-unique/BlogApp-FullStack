366b54ce07f70b3ed1206e9ae3367db1c44680bb
Dependra Kumar <dependragitcreator@gmail.com>
2024-07-08 23:43:33 +0530

    validate email and password in backend

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,33 @@ import "dotenv/config"
 const app = express();
 let port = process.env.PORT || 3000;
 
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;   //regex for email
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;    //regex for password
+
+app.use(express.json());
 mongoose.connect(process.env.DB_LOCATION, {
-    autoIndex: true
+    autoIndex: true,
+        
 })
 
+
 app.post("/signup", (req, res) => {
-    res.json(req.body);
+    
+    let {fullname, email, password} = req.body;
+
+    if(fullname.length < 3){
+        return res.status(403).json({"error": "fullname must be atleast 3 character long"});
+    }
+    if(!email.length){
+        return res.status(403).json({"error": "email is required"});
+    }
+    if(!emailRegex.test(email)){
+        return res.status(403).json({"error": "email invalid"});
+    }
+    if(!passwordRegex.test(password)){
+        return res.status(403).json({"error": "invalid password"})
+    }
+    res.status(200).json({"status" : "okey"});
 })
 
 app.listen(port, () => {
