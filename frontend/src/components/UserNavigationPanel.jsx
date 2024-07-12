699cd02cfaecf64505fc33ca78b45d0696629f88
Dependra Kumar <dependragitcreator@gmail.com>
2024-07-13 00:05:25 +0530

    UserNavigationPanel setup links

---
+++ frontend/src/components/UserNavigationPanel.jsx
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react'
+import PageAnimationWrapper from '../common/PageAnimationWrapper.jsx'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../App.jsx'
+
+function UserNavigationPanel() {
+
+    const {userAuth : {username} } = useContext(UserContext);
+  return (
+    <PageAnimationWrapper 
+    className="absolute right-0 z-50"
+    transition={ {duration: 0.2}}
+    >
+
+        <div className='bg-white absolute right-0 border boder-grey w-60 duration-200'>
+            <Link to="/editor" className='flex gap-2 link md:hidden pl-8 py-4'>
+                <i className="fi fi-rr-edit"></i>
+                <p>Write</p>
+            </Link>
+
+            <Link to={`/user/${username}`} className='flex link pl-8 py-4'>
+                Profile
+            </Link>
+
+            <Link to="/dashboard/blogs" className='flex link pl-8 py-4'>
+                Dashboard
+            </Link>
+            <Link to="/settings/edit-profile" className='flex link pl-8 py-4'>
+                Settings
+            </Link>
+
+            <span className='absolute border-t border-grey w-[100%]'>
+
+            </span>
+        </div>
+
+    </PageAnimationWrapper>
+  )
+}
+
+export default UserNavigationPanel
--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Logo from '../imgs/youtube2 logo.jpg';
 import { Link, Outlet } from 'react-router-dom';
+import { UserContext } from '../App.jsx';
+import UserNavigationPanel from './UserNavigationPanel.jsx';
 
 function Navbar() {
 
     const [searchBoxVisibility , setSearchBoxVisibility] = useState(false);
+
+    const {userAuth, userAuth: {accessToken, profileImg}} = useContext(UserContext)
 
   return (
         <>
@@ -37,12 +41,36 @@ function Navbar() {
                         <p>Write</p>
                     </Link>
 
-                    <Link to="/signin" className='btn-dark py-2 px-4'>
-                        Sign In
-                    </Link>
-                    <Link to="/signup" className='btn-light py-2 px-4 hidden md:block'>
-                        Sign Up
-                    </Link>
+
+                    {
+                        accessToken ?
+                        <>
+                            <Link to="/dashboard/notification">
+                                <button className='w-12 h-12 rounded-full bg-grey relative hover:bg-black/10'>
+                                    <i className="fi fi-rr-bell text-2xl block mt-1"></i>
+                                </button>
+                            </Link>
+
+                            <div className='relative'>
+                                <button className='w-12 h-12 mt-1'> 
+                                    <img src={profileImg} className='w-full h-full object-cover rounded-full' />
+                                </button>
+
+                                <UserNavigationPanel />
+                            </div>
+                        </>
+                        :
+                        <>
+                            <Link to="/signin" className='btn-dark py-2 px-4'>
+                                Sign In
+                            </Link>
+                            <Link to="/signup" className='btn-light py-2 px-4 hidden md:block'>
+                                Sign Up
+                            </Link>
+                        </>
+                    }
+
+                    
                 </div>
             </nav>
 
